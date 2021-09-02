d0e1dfb2d9dd3fb2a2fc12a640617af0b7a0e5e9
Becca-Chae <dabin.chae55@gmail.com>
2021-09-02 16:10:13 -0700

    final changes to utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
-// function product (x, y) {
-//   return x * y;
-// }
+function product (x, y) {
+  return x * y;
+}
 
-// function divide (x, y) {
-//   return x / y ;
-// }
+function divide (x, y) {
+  return x / y ;
+}
 
 function power (x, y) {
   return x ** y ;
